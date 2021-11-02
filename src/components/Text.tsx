9d5aaa5366f629b19beacaa32629b934ad9eda6b
nrsimonelli <nrsimonelli@gmail.com>
2021-11-01 22:26:43 -0500

    case variant

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -4,43 +4,53 @@ export const Text = styled('span', {
   // Reset
   lineHeight: '1',
   margin: '0',
-  fontWeight: '$1',
   fontVariantNumeric: 'tabular-nums',
   display: 'block',
 
   variants: {
+    weight: {
+      1: {
+        fontWeight: '$1',
+      },
+      2: {
+        fontWeight: '$2',
+      },
+      3: {
+        fontWeight: '$3',
+      },
+    },
     size: {
-      '1': {
+      1: {
         fontSize: '$1',
       },
-      '2': {
+      2: {
         fontSize: '$2',
       },
-      '3': {
+      3: {
         fontSize: '$3',
       },
-      '4': {
+      4: {
         fontSize: '$4',
       },
-      '5': {
+      5: {
         fontSize: '$5',
         letterSpacing: '-.015em',
       },
-      '6': {
+      6: {
         fontSize: '$6',
         letterSpacing: '-.016em',
       },
-      '7': {
+      7: {
         fontSize: '$7',
         letterSpacing: '-.031em',
         textIndent: '-.005em',
       },
-      '8': {
+      8: {
         fontSize: '$8',
         letterSpacing: '-.034em',
         textIndent: '-.018em',
       },
-      '9': {
+      9: {
         fontSize: '$9',
         letterSpacing: '-.055em',
         textIndent: '-.025em',
@@ -63,9 +73,27 @@ export const Text = styled('span', {
         color: '$hiContrast',
       },
     },
+    case: {
+      uppercase: {
+        textTransform: 'uppercase',
+      },
+      lowercase: {
+        textTransform: 'lowercase',
+      },
+      capitalize: {
+        textTransform: 'capitalize',
+      },
+      initial: {
+        textTransform: 'initial',
+      },
+      inherit: {
+        textTransform: 'inherit',
+      },
+    },
   },
   defaultVariants: {
     size: '3',
+    weight: '1',
     color: 'contrast',
   },
 });
